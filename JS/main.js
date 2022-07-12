2fbeef9b211db9f158ee5520289e1c346843e23b
Nicolas Gallardo <gallardo.3853@gmail.com>
2022-07-11 21:18:19 -0300

    Proyecto final 11.7

---
+++ JS/main.js
@@ -0,0 +1,28 @@
+// Variables
+
+
+// Main
+let temperatura1 = prompt("Ingrese el número");
+
+for(i=0;i<5;)
+{
+    if (temperatura1 % 2 == 0)
+    {
+        alert ("el número es par");
+    }
+
+    else{
+        alert ("el número es impar");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
